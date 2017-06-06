6a0e392b6b2847b14187cbfee491a09d0f81f878
Alistair Culf <a_culf@hotmail.com>
2017-06-06 10:40:23 +0100

    development version message displayed

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Debug.Print("hello world")
+        Debug.Print("hello world development version")
     End Sub
 End Class
