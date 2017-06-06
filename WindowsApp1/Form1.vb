7531f1b2cce45e3b6e85177862e34127b18c58e6
Alistair Culf <a_culf@hotmail.com>
2017-06-06 10:16:45 +0100

    First commit

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Debug.Print("hello")
+        Debug.Print("hello world")
     End Sub
 End Class
